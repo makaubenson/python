2d596626b51e0c0fb23bb5e9a7060b3f953bf46f
Benson <bensonmakau2000@gmail.com>
2022-02-25 22:28:50 +0300

    lists

--- first.py
+++ first.py
@@ -283,16 +283,17 @@
 #     print("There were", count, 'subject lines in', fname)
 
 # EXCEPTION HANDLING
-fname = input("Enter the file name: ")
+# fname = input("Enter the file name: ")
     
-try:
-  fhand = open(fname)
-except:
-  print("File cannot be opened!", fname)
-  quit()
-count = 0
-for line in fhand:
-    if line.startswith('Subject'):
-        count = count + 1
-    
-    print("There were", count, 'subject lines in', fname)
+# try:
+#   fhand = open(fname)
+# except:
+#   print("File cannot be opened!", fname)
+#   quit()
+# count = 0
+# for line in fhand:
+#     if line.startswith('Subject'):
+#         count = count + 1
+#     print("There were", count, 'subject lines in', fname)
+
+#LISTS
