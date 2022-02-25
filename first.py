47991e7a4e9c1742d50831e63d179d3d5dcb1d2c
Benson <bensonmakau2000@gmail.com>
2022-02-26 00:33:44 +0300

    gurdian pattern

--- first.py
+++ first.py
@@ -368,17 +368,27 @@
 # stuff = abc.split()
 # print(stuff)
 
-han = open('mbox.txt')
+# han = open('mbox.txt')
+# for line in han:
+#     line = line.rstrip()
+#     print("Line",line)
+#     if line == '':
+#         print("Skip Blank")
+#         continue
+#     wds = line.split()
+#     print("Words: ",wds)
+#     if wds[0] != 'From':
+#         print("Ignore")
+#         continue
+#     print(wds[2])
 
+han = open('mbox.txt')
 for line in han:
     line = line.rstrip()
-    print("Line",line)
-    if line == '':
-        print("Skip Blank")
-        continue
     wds = line.split()
-    print("Words: ",wds)
+    #Guardian a bit stronger
+    if len(wds) < 3:
+        continue
     if wds[0] != 'From':
-        print("Ignore")
         continue
     print(wds[2])
